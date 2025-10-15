27f1d9983e5096264e9c2120aedf697a440551db
hadadx <oron.hadadx@gmail.com>
2025-10-16 00:21:25 +0300

    Update ulancher.sh

--- ulancher.sh
+++ ulancher.sh
@@ -1,18 +1,5 @@
-#!/bin/bash
-# 🧠 הגדרת קיצור Super+Space לפתיחת Ulauncher ב-GNOME
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source "[]"
-gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "[]"
-# מסלול לקיצור במערכת ההגדרות
 custom0="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-
-# מוסיף את הקיצור לרשימת הקיצורים המוגדרים
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['${custom0}']"
-
-# הגדרות הקיצור
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom0} name 'Launch Ulauncher'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom0} command 'ulauncher-toggle'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom0} command '/usr/bin/ulauncher-toggle'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${custom0} binding '<Super>space'
-
-echo "✅ Ulauncher הוגדר לפעול עם Super+Space"
-
-
