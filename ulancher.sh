fa14be2d71b550ccd19f0789906425feb3037b62
hadadx <oron.hadadx@gmail.com>
2025-10-16 13:07:04 +0300

    Update ulancher.sh

--- ulancher.sh
+++ ulancher.sh
@@ -1,2 +1,27 @@
 hide_window_decorations yes
  PS1="$  $"
+
+
+background #1a1a1a
+foreground #d0d0d0
+
+color0  #0f0f0f
+color8  #3a3a3a
+color1  #595959
+color9  #777777
+color2  #888888
+color10 #aaaaaa
+color3  #b5b5b5
+color11 #cfcfcf
+color4  #d8d8d8
+color12 #e3e3e3
+color5  #efefef
+color13 #f5f5f5
+color6  #c0c0c0
+color14 #f8f8f8
+color7  #e0e0e0
+color15 #ffffff
+
+cursor #ffffff
+selection_background #e0e0e0
+selection_foreground #000000
